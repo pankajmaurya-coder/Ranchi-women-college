5645ba92c778d33b0859e0371dd7f053952f4887
pankaj kumar <pm631870@gmail.com>
2026-07-06 12:47:57 +0530

    header

--- resources/views/web/layouts/header.blade.php
+++ resources/views/web/layouts/header.blade.php
@@ -1,9 +1,9 @@
-<header class="header">
+<header class="site-header">
     <div class="header-top">
         <div class="container">
             <ul class="top-info">
                 <li>📞 Admission: +91 70501 74723</li>
-                <li><i class="fa-regular fa-envelope" style="color: rgb(255, 255, 255);"></i> ranchiwomenscollege@gmail.com</li>
+                <li><i class="fa-regular fa-envelope"></i> ranchiwomenscollege@gmail.com</li>
                 <li>☎ Helpline: 0651-2562221</li>
             </ul>
         </div>
@@ -15,120 +15,118 @@
                 <div class="header-logo">
                     <div class="logo">
                         <img src="{{ asset('asset/web/layouts/header/logo.png') }}"
-                            alt="Ranchi Women's College Logo">
-                        <span class="logo-estd">
-                            ESTD. 1949
-                        </span>
+                             alt="Ranchi Women's College Logo">
+                        <span class="logo-estd">ESTD. 1949</span>
                     </div>
+
                     <div class="logo-title">
-                        <h1>
-                            Ranchi Women's College
-                        </h1>
-                        <p>
-                            A Constituent College of Ranchi University
-                        </p>
+                        <h1>Ranchi Women's College</h1>
+                        <p>A Constituent College of Ranchi University</p>
                     </div>
                 </div>
 
                 <div class="header-actions">
-                    <img
-                        src="{{ asset('asset/web/layouts/header/naac.png') }}"
-                        alt="NAAC Accreditation"
-                        class="naac-logo">
+                    <img src="{{ asset('asset/web/layouts/header/naac.png') }}"
+                         alt="NAAC Accreditation"
+                         class="naac-logo">
+
                     <a href="#" class="btn btn-outline-primary">
                         Login Institutional Webmail
                     </a>
 
-                     <div class="header-bar"  id="menuToggle"><i class="fa-solid fa-bars" style="color: rgb(0, 0, 0);"></i></div>
+                    <button class="header-bar"
+                            id="menuToggle"
+                            aria-label="Toggle Navigation"
+                            aria-controls="navbar"
+                            aria-expanded="false"
+                            type="button">
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 </header>
- <nav class="header-navbar" id="navbar">
-    <div class="container">
-        <ul class="navbar-menu">
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li class="nav-dropdown">
-                <a href="#">
-                    About
-                    <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="nav-dropdown-menu">
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Background</a></li>
-                    <li><a href="#">Vision & Mission</a></li>
-                    <li><a href="#">Principals</a></li>
-                    <li><a href="#">Rules & Regulations</a></li>
-                    <li><a href="#">Holiday List</a></li>
-                    <li><a href="#">Ranchi University Kulgeet</a></li>
-                    <li><a href="#">IQAC & AQAR</a></li>
-                    <li><a href="#">Statutory Bodies</a></li>
-                    <li><a href="#">Committees</a></li>
-                    <li><a href="#">Infrastructure</a></li>
-                    <li><a href="#">Newsletter</a></li>
-                </ul>
-            </li>
-            <li class="nav-dropdown">
-                <a href="#">Academics
-                     <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="nav-dropdown-menu">
-                       <li><a href="#">Syllabus</a></li>
-                       <li><a href="#">Programme and Cource</a></li>
-                       <li><a href="#">Academic Calender</a></li>
-                       <li><a href="#">Library</a></li>
-                       <li><a href="#">Fee Structure</a></li>
-                       <li><a href="#">Placement</a></li>
-                        <li><a href="#">Distinguished Alumni</a></li>
-                </ul>
-            </li>
-            <li class="nav-dropdown">
-                <a href="#">Admissions
-                    <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="nav-dropdown-menu">
-                       <li><a href="#">UG Admission</a></li>
-                       <li><a href="#">UG Admission</a></li>
-                       <li><a href="#">Vocational Admission</a></li>
-                       <li><a href="#">B.Ed Admission</a></li>
-                </ul>
-            </li>
-            <li class="nav-dropdown">
-                <a href="#">Department
-                     <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="nav-dropdown-menu">
-                       <li><a href="#">Art</a></li>
-                       <li><a href="#">Science</a></li>
-                       <li><a href="#">M.A In Education</a></li>
-                       <li><a href="#">Commerce</a></li>
-                       <li><a href="#">Vocational</a></li>
-                       <li><a href="#">Department of B.Ed</a></li>
-                        <li><a href="#">Distinguished Alumni</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Examination</a>
-            </li>
+    <nav class="header-navbar" id="navbar">
+            <div class="container">
+                <ul class="navbar-menu">
 
-            <li>
-                <a href="#">Gallery</a>
-            </li>
-            <li class="nav-dropdown">
-                <a href="#">RTI
-                <i class="fa-solid fa-angle-down"></i>
-                </a>
-                 <ul class="nav-dropdown-menu">
-                      <li><a href="#">Gravience</a></li>
-                       <li><a href="#">Action Taken Report</a></li>
-                 </ul>
-            </li>
-            <li>
-                <a href="#">IQAC / NAAC</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+                    <li><a href="#">Home</a></li>
+
+                    <li class="nav-dropdown">
+                        <a href="#">About <i class="fa-solid fa-angle-down"></i></a>
+                        <ul class="nav-dropdown-menu">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Background</a></li>
+                            <li><a href="#">Vision & Mission</a></li>
+                            <li><a href="#">Principals</a></li>
+                            <li><a href="#">Rules & Regulations</a></li>
+                            <li><a href="#">Holiday List</a></li>
+                            <li><a href="#">Ranchi University Kulgeet</a></li>
+                            <li><a href="#">IQAC & AQAR</a></li>
+                            <li><a href="#">Statutory Bodies</a></li>
+                            <li><a href="#">Committees</a></li>
+                            <li><a href="#">Infrastructure</a></li>
+                            <li><a href="#">Newsletter</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-dropdown">
+                        <a href="#">Academics <i class="fa-solid fa-angle-down"></i></a>
+                        <ul class="nav-dropdown-menu">
+                            <li><a href="#">Syllabus</a></li>
+                            <li><a href="#">Programme and Course</a></li>
+                            <li><a href="#">Academic Calendar</a></li>
+                            <li><a href="#">Library</a></li>
+                            <li><a href="#">Fee Structure</a></li>
+                            <li><a href="#">Placement</a></li>
+                            <li><a href="#">Distinguished Alumni</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-dropdown">
+                        <a href="#">Admissions <i class="fa-solid fa-angle-down"></i></a>
+                        <ul class="nav-dropdown-menu">
+                            <li><a href="#">UG Admission</a></li>
+                            <li><a href="#">PG Admission</a></li>
+                            <li><a href="#">Vocational Admission</a></li>
+                            <li><a href="#">B.Ed Admission</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-dropdown">
+                        <a href="#">Department <i class="fa-solid fa-angle-down"></i></a>
+                        <ul class="nav-dropdown-menu">
+                            <li><a href="#">Arts</a></li>
+                            <li><a href="#">Science</a></li>
+                            <li><a href="#">M.A. in Education</a></li>
+                            <li><a href="#">Commerce</a></li>
+                            <li><a href="#">Vocational</a></li>
+                            <li><a href="#">Department of B.Ed</a></li>
+                            <li><a href="#">Distinguished Alumni</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">Examination</a></li>
+                    <li><a href="#">Gallery</a></li>
+
+                    <li class="nav-dropdown">
+                        <a href="#">RTI <i class="fa-solid fa-angle-down"></i></a>
+                        <ul class="nav-dropdown-menu">
+                            <li><a href="#">Grievance</a></li>
+                            <li><a href="#">Action Taken Report</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">IQAC / NAAC</a></li>
+
+                    <li class="mobile-login">
+    <a href="#">
+        <i class="fa-solid fa-right-to-bracket"></i>
+        Login Institutional Webmail
+    </a>
+</li>
+
+                </ul>
+            </div>
+        </nav>
